47f69a4b7a072c014a2e049a4ffce91dc6a0a542
eypklc <ekilic3844@gmail.com>
2023-12-14 00:21:19 +0300

    features directory added

--- src/test/java/com/zerobank/runners/CukesRunner.java
+++ src/test/java/com/zerobank/runners/CukesRunner.java
@@ -18,7 +18,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/sword/step_definitions",
         dryRun = false,
-        tags="@SCRUM-1251"
+        tags=""
 )
 
 public class CukesRunner {
